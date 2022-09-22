f781157dce6db57d35b1994ba045c7af4a3a111c
Dsfr62 <saviodudu79@gmail.com>
2022-09-22 12:34:22 -0300

    Started the drinks static page

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
+    const pathName = window.location.pathname;
 
     function handleNav() {
         setNav(!nav);
@@ -11,23 +12,39 @@ const Navbar = () => {
     //TODO: Define the icon of the options
     return (
         <div className="bg-firstBrown flex h-20 justify-between items-center p-6 text-white">
-            <p className="font-semibold">Drink Logo</p>
+            <p className="font-semibold"><Link to="/">Drink Logo</Link></p>
             
             {
             !nav ? 
             <div className="flex">
-                <p className="invisible md:visible md:mx-6 hover:bg-firstNuts hover:cursor-pointer hover:rounded-sm ease-linear transition-all"><Link to="drinks">Drinks</Link></p>
-                <p className="invisible md:visible md:mx-6 hover:bg-firstNuts hover:cursor-pointer hover:rounded-sm ease-linear transition-all"><Link to="games">Games</Link></p>
-                <p className="invisible md:visible md:ml-6 hover:bg-firstNuts hover:cursor-pointer hover:rounded-sm ease-linear transition-all"><Link to="profile">Profile</Link></p>
-                <button className="visible md:invisible m-6 md:m-0 focus:outline-none focus:bg-firstNuts ease-linear transition-all" onClick={handleNav}>Click</button>
+                <p className="invisible md:visible md:mx-6 hover:bg-firstNuts hover:cursor-pointer hover:rounded-sm ease-linear transition-all">
+                    <Link to={pathName == "/drinks" ? "" : "/drinks"}>Drinks</Link>
+                </p>
+                <p className="invisible md:visible md:mx-6 hover:bg-firstNuts hover:cursor-pointer hover:rounded-sm ease-linear transition-all">
+                    <Link to={pathName == "/games" ? "" : "/games"}>Games</Link>
+                </p>
+                <p className="invisible md:visible md:ml-6 hover:bg-firstNuts hover:cursor-pointer hover:rounded-sm ease-linear transition-all">
+                    <Link to={pathName == "/profile" ? "" : "/profile"}>Profile</Link>
+                </p>
+                <button className="visible md:invisible m-6 md:m-0 focus:outline-none focus:bg-firstNuts ease-linear transition-all" onClick={handleNav}>
+                    Click
+                </button>
             </div>
             :
             <div className="flex flex-col">
-                <button className="visible md:invisible focus:outline-none focus:bg-firstNuts ease-linear transition-all m-4 mt-40 animate-pulse" onClick={handleNav}>Close</button>
+                <button className="visible md:invisible focus:outline-none focus:bg-firstNuts ease-linear transition-all m-4 mt-40 animate-pulse" onClick={handleNav}>
+                    Close
+                </button>
                 <div className="bg-firstBrown flex flex-col items-center p-2 rounded">
-                    <p className="border-t-2 border-firstNuts cursor-pointer ease-linear transition-all p-1.5 my-0.5"><Link to="drinks">Drinks</Link></p>
-                    <p className="border-t-2 border-firstNuts cursor-pointer ease-linear transition-all p-1.5 my-0.5"><Link to="games">Games</Link></p>
-                    <p className="border-t-2 border-firstNuts cursor-pointer ease-linear transition-all p-1.5 my-0.5"><Link to="profile">Profile</Link></p>
+                    <p className="border-t-2 border-firstNuts cursor-pointer ease-linear transition-all p-1.5 my-0.5">
+                        <Link to={pathName == "/drinks" ? "" : "/drinks"}>Drinks</Link>
+                    </p>
+                    <p className="border-t-2 border-firstNuts cursor-pointer ease-linear transition-all p-1.5 my-0.5">
+                        <Link to={pathName == "/games" ? "" : "/games"}>Games</Link>
+                    </p>
+                    <p className="border-t-2 border-firstNuts cursor-pointer ease-linear transition-all p-1.5 my-0.5">
+                        <Link to={pathName == "/profile" ? "" : "/profile"}>Profile</Link>
+                    </p>
                 </div>
             </div>
             }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import DrinksPage from "./pages/drinks-page";
 import HomePage from "./pages/home-page";
 
 const AppRoutes = () => {
@@ -6,6 +7,7 @@ const AppRoutes = () => {
 
         <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/drinks" element={<DrinksPage />} />
         </Routes>
 
     );
